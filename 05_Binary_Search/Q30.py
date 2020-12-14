af51da5b859ff0115ec61700f5fa8b9ca9a7717c
kesol0328 <kesol0328@gmail.com>
2020-12-14 16:21:53 +0900

    Q30 가사 검색 문제, 풀이 추가

--- 05_Binary_Search/Q30.py
+++ 05_Binary_Search/Q30.py
@@ -33,4 +33,40 @@ words	                                                queries	                  
 "fr???"는 "frodo", "front", "frost", "frame"에 매치되므로 4입니다.
 "fro???"는 "frozen"에 매치되므로 1입니다.
 "pro?"는 매치되는 가사 단어가 없으므로 0 입니다.
+
+https://programmers.co.kr/learn/courses/30/lessons/60060
 '''
+
+from bisect import bisect_left, bisect_right
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
+# 모든 단어를 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words: # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
+        array[len(word)].append(word) # 단어를 삽입
+        reversed_array[len(word)].append(word[::-1]) # 단어를 뒤집어서 삽입
+    
+    for i in range(10001): # 이진 탐색을 수행하기 위해 각 단어 리스트 정렬 수행
+        array[i].sort()
+        reversed_array[i].sort()
+    
+    for q in queries: # 쿼리를 하나씩 확인하며 처리
+        if q[0] != '?': # 접미사에 와일드카드가 붙은 경우
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else: # 접두사에 와일드카드가 붙은 경우
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        
+        # 검색된 단어의 개수를 저장
+        answer.append(res)
+    
+    return answer
