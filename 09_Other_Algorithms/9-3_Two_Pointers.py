800daa7856eb0ffabcf5c2377ecdd9fe0d16b5f9
kesol0328 <kesol0328@gmail.com>
2020-12-23 11:41:57 +0900

    two pointers 추가

--- 09_Other_Algorithms/9-3_Two_Pointers.py
+++ 09_Other_Algorithms/9-3_Two_Pointers.py
@@ -9,5 +9,25 @@
 3. 현재 부분합이 M보다 작으면 end를 1 증가시킨다.
 4. 현재 부분합이 M보다 크거나 같으면 start를 1 증가시킨다.
 5. 모든 경우를 확인할 때까지 2번부터 4번까지의 과정을 반복한다.
-
 '''
+
+n = 5 # 데이터의 개수 N
+m = 5 # 찾고자 하는 부분합 M
+data = [1, 2, 3, 2, 5] # 전체 수열
+
+count = 0
+interval_sum = 0
+end = 0
+
+# start를 차례대로 증가시키며 반복
+for start in range(n):
+    # end를 가능한 만큼 이동시키기
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    # 부분합이 m일 때 카운트 증가
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+
+print(count)
